13458b8ef1db07c770eb9b28615e09cff958ec4e
Roy Eyal <roy.eyal@gmail.com>
2024-08-21 11:16:22 +0300

    Sticky navbar

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # priory-team
 CSS and JS for the the Priory-Team website
+
+Code relies on GSAP, so load first:
+
+1. GSAP
+2. GSAP ScrollTrigger
+3. GSAP SplitText
+
+[Direct link to main branch of JS file](https://cdn.jsdelivr.net/gh/royeyal/priory-team@main/priory-team.js)
+
+[Direct link to main branch of CSS file](https://cdn.jsdelivr.net/gh/royeyal/priory-team@main/priory-team.css)
---
+++ priory-team.js
@@ -0,0 +1,45 @@
+ScrollTrigger.create({
+	start: "top -80",
+	end: 99999,
+	toggleClass: {
+		className: "is-sticky",
+		targets: ".navbar",
+	},
+});
+
+function initializeSplitText() {
+	const titles = document.querySelectorAll('h1, h2, .text-size-xlarge, .qoute');
+	titles.forEach((title) => {
+		const split = new SplitText(title, { type: "lines", linesClass: "split-line" });
+		split.lines.forEach((line) => {
+			const wrapper = document.createElement('div');
+			wrapper.style.overflow = 'hidden';
+			line.parentNode.insertBefore(wrapper, line);
+			wrapper.appendChild(line);
+		});
+
+		const tl = gsap.timeline({
+			paused: true
+		});
+
+		tl.from(split.lines, {
+			duration: 1.5,
+			yPercent: 150,
+			ease: "power4.out",
+			skewY: -3,
+			stagger: 0.1,
+		});
+
+		ScrollTrigger.create({
+			trigger: title,
+			start: "top 100%",
+			end: "bottom 0%",
+			animation: tl,
+			toggleActions: "play none play none", //onEnter, onLeave, onEnterBack, onLeaveBack
+		});
+	});
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	initializeSplitText();
+});
